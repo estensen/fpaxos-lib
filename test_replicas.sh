2948c52dbacaeff290a2ee9deb8ee22c64bb5f33
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-03-06 15:13:22 -0800

    Modify config file and test script for OpenStack

--- test_replicas.sh
+++ test_replicas.sh
@@ -5,7 +5,5 @@ echo 'started 0'
 echo 'started 1'
 ./build/sample/replica 2 ./paxos.conf > 2.log &
 echo 'started 2'
-./build/sample/replica 3 ./paxos.conf > 3.log &
-echo 'started 3'
 sleep 2
 ./build/sample/client ./paxos.conf -p 0 -o 10 &
